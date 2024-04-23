a0f7c672b5cbb529f1eb08ca0e39757eb4534f4f
Dhanush <40487848+Dhanush623@users.noreply.github.com>
2024-04-23 20:04:10 +0530

    login assessment added

---
+++ assessment/login.js
@@ -0,0 +1,21 @@
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    var users = [
+        {"username": "john", "password": "johnsmith", "display": "John Smith", "id": 1},
+        {"username": "emily", "password": "emily@johnson","display":  "Emily Johnson", "id": 2},
+        {"username": "brown", "password": "brown#321","display":  "Michael Brown", "id": 3},
+        {"username": "williams", "password": "williams#007", "display": "Sarah Williams", "id": 4},
+        {"username": "david", "password": "david@123", "display": "David Jones", "id": 5},
+    ]
+    
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    console.log("username ", username)
+    console.log("password ", password)
+    var user = users.find(u => u.username === username && u.password === password);
+    if (user) {
+        alert("Login successful!");
+    } else {
+        alert("Invalid username or password");
+    }
+});
