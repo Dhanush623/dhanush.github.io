b9e6435a386f5dfd860674c943ae1e2ecb6e9ff9
Dhanush Rajamohan <dhanush623@gmail.com>
2024-07-23 14:53:39 +0530

    redeploy

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -11,7 +11,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // Initialize ScreenUtil with context
     ScreenUtil.init(context);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
